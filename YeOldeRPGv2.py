6802181285201066d078c48fabf00522e0fdc186
yeoldeknob <tmckenna1988@gmail.com>
2021-05-26 12:42:32 -0400

    Update YeOldeRPGv2.py

--- YeOldeRPGv2.py
+++ YeOldeRPGv2.py
@@ -41,6 +41,57 @@ class Player:
         self.skill_2 = ''
         self.skill_3 = ''
 
+class Warrior:
+    def roll_warrior(self):
+        myPlayer.role = "Warrior"
+        myPlayer.hp = Dice.d_20()
+        myPlayer.mp = Dice.d_6()
+        myPlayer.blk = Dice.d_8()
+        myPlayer.atk = Dice.d_12()
+        total_skill_points = (myPlayer.hp + myPlayer.mp + myPlayer.blk + myPlayer.atk)
+        if total_skill_points >= 25 or total_skill_points <= 20:
+            Warrior.roll_warrior(self)
+        else:
+            return
+
+class Mage:
+    def roll_mage(self):
+        myPlayer.role = "Mage"
+        myPlayer.hp = Dice.d_6()
+        myPlayer.mp = Dice.d_10()
+        myPlayer.blk = Dice.d_4()
+        myPlayer.atk = Dice.d_10()
+        total_skill_points = (myPlayer.hp + myPlayer.mp + myPlayer.blk + myPlayer.atk)
+        if total_skill_points >= 25 or total_skill_points <= 20:
+            Mage.roll_mage(self)
+        else:
+            return
+
+class Rogue:
+    def roll_rogue(self):
+        myPlayer.role = "Rogue"
+        myPlayer.hp = Dice.d_8()
+        myPlayer.mp = Dice.d_8()
+        myPlayer.blk = Dice.d_10()
+        myPlayer.atk = Dice.d_6()
+        total_skill_points = (myPlayer.hp + myPlayer.mp + myPlayer.blk + myPlayer.atk)
+        if total_skill_points >= 25 or total_skill_points <= 20:
+            Rogue.roll_rogue(self)
+        else:
+            return
+
+class Barb:
+    def roll_barb(self):
+        myPlayer.role = "Barbarian"
+        myPlayer.hp = Dice.d_12()
+        myPlayer.mp = Dice.d_6()
+        myPlayer.blk = Dice.d_6()
+        myPlayer.atk = Dice.d_8()
+        total_skill_points = (myPlayer.hp + myPlayer.mp + myPlayer.blk + myPlayer.atk)
+        if total_skill_points >= 25 or total_skill_points <= 20:
+            Barb.roll_barb(self)
+        else:
+            return
 
 myPlayer = Player()
 
@@ -57,7 +108,7 @@ class Enemy:
 
 Enemy.nick_the_scav = Enemy("Nick the Scav", Dice.d_6(), Dice.d_4(), Dice.d_4(), Dice.d_4())
 Enemy.shturman = Enemy("Shturman", Dice.d_12(), Dice.d_6(), Dice.d_6(), Dice.d_6())
-Enemy.
+Enemy.hitch = Enemy("Hitch the Fan", Dice.d_6(), Dice.d_4(), Dice.d_4(), Dice.d_4())
 
 
 ### Main Menu ###
@@ -85,55 +136,44 @@ class Menu:
 class Setup:
     def player_setup_name(self):
         print("\nWhat is your name?")
-        myPlayer.name = input(">>> ")
+        myPlayer.name = input(">>> ").title()
         Setup.player_setup_class(self)
 
     def player_setup_class(self):
-        print("Are you a (W)arrior or a (M)age?")
+        print("Are you a (W)arrior, (M)age, (R)ogue, or (B)arbarian?")
         answer_2 = input("\n>>> ")
         if "w" in answer_2.lower():
-            myPlayer.role = 'Warrior'
-            myPlayer.hp = Dice.d_20()
-            myPlayer.mp = Dice.d_6()
-            myPlayer.blk = Dice.d_8()
-            myPlayer.atk = Dice.d_12()
-            myPlayer.skill_1 = "Shield Bash"
-            myPlayer.skill_2 = "Double Suplex"
+            Warrior.roll_warrior(self)
             Setup.confirmation(self)
         elif "m" in answer_2.lower():
-            myPlayer.role = 'Mage'
-            myPlayer.hp = Dice.d_6()
-            myPlayer.mp = Dice.d_10()
-            myPlayer.blk = Dice.d_6()
-            myPlayer.atk = Dice.d_8()
-            myPlayer.skill_1 = "Fireball"
-            myPlayer.skill_2 = "Ice Shard"
-            myPlayer.skill_3 = "Healing"
+            Mage.roll_mage(self)
+            Setup.confirmation(self)
+        elif "r" in answer_2.lower():
+            Rogue.roll_rogue(self)
+            Setup.confirmation(self)
+        elif "b" in answer_2.lower():
+            Barb.roll_bard(self)
             Setup.confirmation(self)
         else:
             self.player_setup_class()
 
     def confirmation(self):
-        print("\nSo, you are " + myPlayer.name + " the " + myPlayer.role + "?")
-        print("\n#############################")
+        print("\n" + myPlayer.name + " the " + myPlayer.role)
+        print("#############################")
         print("Hit Points: " + str(myPlayer.hp))
         print("Mana Points: " + str(myPlayer.mp))
         print("Defence: " + str(myPlayer.blk))
         print("Attack Points: " + str(myPlayer.atk))
 
-        if myPlayer.skill_1:
-            print("Skill 1: " + str(myPlayer.skill_1))
-        if myPlayer.skill_2:
-            print("Skill 2: " + str(myPlayer.skill_2))
-        if myPlayer.skill_3:
-            print("Skill 3: " + str(myPlayer.skill_3))
-
         print("\nRe-roll? (Y)es or (N)o")
         answer_3 = input("\n>>> ")
         if "n" in answer_3:
-            ChapterOne.intro_chap_one()
+            ChapterOne.intro_chap_one(self)
         elif "y" in answer_3:
             Setup.player_setup_class(self)
+        else:
+            print("\n\nYou have to choose (Y)es or (N)o")
+            Setup.confirmation(self)
 
 
 ### The game starts here ###
